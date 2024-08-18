0e8658f7b722bb8569bc21366f4a9530bbe52f2b
chuchu3005 <chuminhtuann97@gmail.com>
2024-08-18 16:54:35 +0700

    update btap html form

--- Tổng quan JavaS/FuntionKQ.js
+++ Tổng quan JavaS/FuntionKQ.js
@@ -1,1 +1,17 @@
-function ketqua(){}
+function ketqua() {
+    let aElement = document.getElementById('a');
+    let bElement = document.getElementById('b');
+    let valueA= aElement.value
+    let valueB= bElement.value
+    if (valueA==0) {
+        if (valueB==0) {
+            alert('phuong trinh vo so nghiem./n');
+        } else {
+            alert('phuong trinh vo nghiem');
+        }
+    }else {
+        x= -valueB/valueA;
+        alert('nghiem cua phuong trinh la: x=' -valueB/valueA)
+    }
+
+}
